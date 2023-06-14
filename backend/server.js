48b041d166337a95f4a1cda1dc54c04c2f1c1a76
newby101 <ishimwekevin45@gmail.com>
2023-06-13 20:56:06 -0400

    last commit

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,24 @@
 import express from "express";
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+import prisma from "./middleware/prismaClient.js";
+import { graphqlHTTP } from "express-graphql";
+import { schema } from "./models/schema.js";
+import { rootFunctions } from "./controllers/root.js";
+import  cors  from "cors"
 const app = express();
-app.use(express.json());
 
+app.use(express.json());
+app.use(cors({ origin: true, credentials: true }));
 app.get("/", async (req, res) => {
   res.send("Home");
 });
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue: rootFunctions,
+    graphiql: true,
+  })
+);
 app.use("*", (req, res) => {
   res.status(404).json({ message: "enpoint not found" });
 });
