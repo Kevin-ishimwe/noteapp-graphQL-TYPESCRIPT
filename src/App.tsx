d908d5522999cedb3a202fa2df37ca8fc9aa459e
newby101 <ishimwekevin45@gmail.com>
2023-06-16 23:22:30 -0400

    adding note

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import desktopdark from "./assets/images/bg-desktop-dark.jpg";
 function App() {
   const [list, setlist] = useState(0);
   const [theme, settheme] = useState("");
+  const [tracker, settracker] = useState(0);
   useEffect(() => {
     const prevtheme = localStorage.getItem("theme");
     if (prevtheme) {
@@ -42,7 +43,7 @@ function App() {
       .then((data) => {
         setlist(data.data.allNotesUser);
       });
-  }, []);
+  }, [tracker]);
   const handleTheme = () => {
     if (theme === "light") {
       localStorage.setItem("theme", "dark");
@@ -54,6 +55,71 @@ function App() {
       document.documentElement.classList.remove("dark");
     }
   };
+  const handleAddNote = async (e) => {
+    console.log(e.target.value);
+    await fetch("http://localhost:2065/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `mutation createNotes {
+            createNote(title: "${e.target.value}", content: "body", authorId: 1,status:"active") {
+          id
+        }
+          }`,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        e.target.value = "";
+        settracker(() => tracker + 1);
+      });
+  };
+
+  const handleDeletenote = async (id) => {
+    await fetch("http://localhost:2065/graphql", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `
+          mutation deletenote {
+            deleteNote (id:${id})
+          }
+        `,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        settracker(() => tracker + 1);
+      });
+  };
+  // complete status update 
+
+  const updateNote=async(id)=>{
+    await fetch("http://localhost:2065/graphql", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `
+          mutation updatenotes {
+            updateStatus(id:${id},status:"completed")
+            }
+        `,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        settracker(() => tracker + 1);
+      });
+
+  }
 
   return (
     <>
@@ -75,14 +141,23 @@ function App() {
           <input
             placeholder="new todo"
             className="bg-[#fafafa] shadow-xl dark:bg-[#25273c] mt-4 py-3 rounded-md text-xl focus:outline-none pl-4"
+            onKeyDown={(e) => {
+              if (e.keyCode === 13) {
+                e.preventDefault();
+                handleAddNote(e);
+              }
+            }}
           />
-          <div className=" rounded-xl bg-[#fafafa] dark:bg-[#25273c] mt-4 min-h-[30vh] shadow-2xl">
+          <div className=" rounded-xl bg-[#fafafa] dark:bg-[#25273c] mt-4 min-h-[30vh] shadow-2xl pb-4">
             {list.notes === undefined ? (
               <h1>no notes yet</h1>
             ) : (
-              list.notes.map(({ title, status }) => {
+              list.notes.map(({ title, status, id }) => {
                 return (
-                  <div className=" flex justify-between py-1.5 border-b-[1.5px] font-semibold text-[large] dark:text-gray-400 text-[#100e18c4] mt-2 list-none  border-gray-200 ">
+                  <div
+                    key={id}
+                    className=" flex justify-between py-1.5 border-b-[1.5px] font-semibold text-[large] dark:text-gray-400 text-[#100e18c4] mt-2 list-none  border-gray-200 "
+                  >
                     <div className="flex">
                       {status === "completed" ? (
                         <p className="line-through">
@@ -93,12 +168,25 @@ function App() {
                         </p>
                       ) : (
                         <p>
-                          <button className="border-[1px] mx-4 border-gray-400 rounded-full h-5 w-5"></button>
+                          <button className="border-[1px] mx-4 border-gray-400 rounded-full h-5 w-5" id={id} onClick={(e)=>{
+                            updateNote(e.target.id);
+                          }} />
                           {title}
                         </p>
                       )}
                     </div>
-                    <RxCross1 className=" mr-4" />
+
+                    <RxCross1
+                      className=" mr-4 hover:text-red-500"
+                      id={id}
+                      onClick={(e) => {
+                        const id = Number(e.target.id);
+                        console.log(id);
+                        typeof id != String
+                          ? handleDeletenote(id)
+                          : console.log(typeof e.target.id);
+                      }}
+                    />
                   </div>
                 );
               })
